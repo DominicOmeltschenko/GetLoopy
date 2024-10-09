b0ba12e7869b12aabdf30137acf40cca3b15785e
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-10-09 14:27:58 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.lang.foreign.SymbolLookup;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int countByOne = 0;
+
+        for (countByOne = 0; countByOne <= 30; countByOne++)
+        {
+            System.out.print(countByOne);
+        }
+    }
+}
