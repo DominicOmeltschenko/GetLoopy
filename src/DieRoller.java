198d681fd4653f8ff0ed7848236aa30f2808d0a0
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-10-09 18:10:06 -0400

    part 8 works now

--- src/DieRoller.java
+++ src/DieRoller.java
@@ -6,9 +6,9 @@ public class DieRoller {
     {
         Scanner in = new Scanner(System.in);
         Random generator = new Random();
-        int dieOne = 0;
-        int dieTwo = 1;
-        int dieThree = 2;
+        int dieOne;
+        int dieTwo;
+        int dieThree;
         int rollNum = 1;
         int sumNum = 0;
         String playAgain = "";
@@ -20,28 +20,29 @@ public class DieRoller {
        {
 
                do {
-                   rollNum = rollNum++;
+                   rollNum++;
                    dieOne = new Random().nextInt(6) + 1;
                    dieTwo = new Random().nextInt(6) + 1;
                    dieThree = new Random().nextInt(6) + 1;
                    sumNum = dieOne + dieTwo + dieThree;
                    System.out.printf("%-5d %1d %4d %4d %4d\n", rollNum, dieOne, dieTwo, dieThree, sumNum);
-               } while (dieOne != dieTwo && dieOne != dieThree);
+               } while (!(dieOne == dieThree && dieOne == dieTwo));
            System.out.print("Would you like to roll again? (Y/N): ");
            playAgain = in.nextLine();
-           if (playAgain.equals("N"))
+           if (playAgain.equalsIgnoreCase("N"))
            {
                done = true;
            }
-           else if (playAgain.equals("Y" ))
+           else if (playAgain.equalsIgnoreCase("Y" ))
            {
              done = false;
            }
            else
            {
-               trash = in.nextLine();
-             System.out.println("You said: " +trash);
-             System.out.println("Please say Y or N");
+               trash = playAgain;
+             System.out.println("You said: " + trash);
+             System.out.print("Please say Y or N: ");
+               playAgain = in.nextLine();
            }
 
        }while(!done);
