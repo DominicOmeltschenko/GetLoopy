c079ad419053bfc4353e0d7e2141f908691ec6cb
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-10-09 16:54:39 -0400

    New code added, make sure to remove all comments before pushing

--- src/DieRoller.java
+++ src/DieRoller.java
@@ -1,6 +1,12 @@
+import java.util.Random;
 public class DieRoller {
-    public static void main(String[] args) {
 
-
+    public static void main(String[] args)
+    {
+        Random generator = new Random();
+        int dieOne = new Random().nextInt(6) + 1;
+        int dieTwo = new Random().nextInt(6) + 1;
+        int dieThree = new Random().nextInt(6) + 1;
+        System.out.println(dieOne+ " " + dieTwo + " " + dieThree);
     }
 }
